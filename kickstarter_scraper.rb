8d170546439345e3fc2adcc0a9e66ed458be0028
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-10 20:21:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,5 +9,13 @@ def create_project_hash
 end
 
 
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image: project.css("div.project-thumbnail a img").attribute("src").value
+# blurb: project.css("p.bbcard_blurb").text
+# location: project.css('span.location-name').text
+# percent_funded: project.css('li.funded').first.css('strong').children.text.gsub("%"," ").to_i
+
+
 
 create_project_hash
