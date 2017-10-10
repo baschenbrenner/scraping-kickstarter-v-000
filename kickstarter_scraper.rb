bc99eaf262464247d73ae3eb167bfe0c81c49a53
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-10 20:25:27 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,19 +11,19 @@ def create_project_hash
      title = project.css("h2.bbcard_name strong a").text
      projects[title.to_sym]={
 
-       
-        :image_link = project.css("div.project-thumbnail a img").attribute("src").value
-        :description = project.css("p.bbcard_blurb").text
-       :location = project.css('span.location-name').text
-        :percent_funded =  project.css('li.funded').first.css('strong').children.text.gsub("%"," ").to_i
+
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value
+        :description => project.css("p.bbcard_blurb").text
+       :location => project.css('span.location-name').text
+        :percent_funded =>  project.css('li.funded').first.css('strong').children.text.gsub("%"," ").to_i
 
 
-       
+
      }
 
 
 
-     
+
    end
 
    projects
